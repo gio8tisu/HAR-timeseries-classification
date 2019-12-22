1a8e520c17599af3446b83a8e39a13f1f282d999
gio8tisu <gio8.tisu@gmail.com>
2019-12-22 21:49:03 +0100

    Add metadata option in classify_time_series

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from statistics import mode
 
-def classify_time_series(ts, clf, cut):
+import numpy as np
+
+
+def classify_time_series(ts, clf, cut, metadata=None):
     serie = []
     for w in range(0, ts.shape[0], cut):
         crop = ts[w:(w + cut)]
-        try:
-            serie.append(clf.predict(crop))
-        except:
-            pass
+        if metadata is not None:
+            crop = np.hstack([crop, metadata])
+        serie.append(clf.predict(crop))
     return mode(serie)
