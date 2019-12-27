a4bf42b5f2f2412d1634410c6db7e366257a8c79
gio8tisu <gio8.tisu@gmail.com>
2019-12-27 12:04:08 +0100

    ADD autocorrelation and fourier transforms

--- transforms.py
+++ transforms.py
@@ -1,6 +1,8 @@
 """Module for data transformation and normalization stuff."""
 
 import numpy as np
+import scipy.signal
+from sklearn.base import TransformerMixin, BaseEstimator
 
 
 def stft(signal: np.ndarray, window_size=256, copy=True, pad=True):
@@ -42,6 +44,102 @@ def stft(signal: np.ndarray, window_size=256, copy=True, pad=True):
         signal_stft[..., c] = np.absolute(np.fft.fft(signal[..., c]))
 
     return signal_stft
+
+
+def autocorrelation_peaks(signal, normalize=True):
+    """
+
+    :param signal: (L, d) numpy array.
+    :param normalize: whether to normalize by value at lag-0 or not.
+    """
+    lag_0 = signal.shape[0] - 1
+    autocorrelations = []
+    for dim in range(signal.shape[1]):
+        # Compute autocorrelation of dimension <dim>.
+        corr = scipy.signal.correlate(signal[:, dim], signal[:, dim])
+        # "Discard" first half.
+        corr = corr[lag_0:]
+        if normalize:
+            # Divide by value at lag-0.
+            corr = corr / corr[0]
+
+        autocorrelations.append(corr)
+
+    return autocorrelations
+
+
+def frequency_peaks(signal):
+    frequencies = []
+    for dim in range(signal.shape[1]):
+        # Compute fft of dimension <dim>.
+        fft = np.fft.fft(signal[:, dim], signal[:, dim])
+        # Only positive frequencies.
+        fft = fft[:(signal.shape[0] / 2)]
+
+        frequencies.append(np.abs(fft))
+
+    return frequencies
+
+
+class NormTransformer(BaseEstimator, TransformerMixin):
+
+    def transform(self, X, y=None, **fit_params):
+        return np.linalg.norm(X, axis=1)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+
+class FourierTransform(BaseEstimator, TransformerMixin):
+
+    def __init__(self, window=None):
+        self.window = window
+
+    def transform(self, X, y=None, **fit_params):
+        # Compute fft of dimension <dim>. Only positive frequencies.
+        if self.window is not None:
+            X = X * self.window
+        fft = np.fft.rfft(X)
+        # Compute absolute value.
+        return np.abs(fft)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+
+class AutocorrelationTransform(BaseEstimator, TransformerMixin):
+
+    def __init__(self, normalize):
+        self.normalize = normalize
+
+    def transform(self, X, y=None, **fit_params):
+        corr = scipy.signal.correlate(X, X)
+        # "Discard" first half.
+        corr = corr[(X.shape[0] - 1):]
+        if self.normalize:
+            # Divide by value at lag_0.
+            corr = corr / corr[0]
+        return corr
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+
+class FindPeaksTransform(BaseEstimator, TransformerMixin):
+
+    def __init__(self, n_peaks, height=0, **scipy_kwargs):
+        self.n_peaks = n_peaks
+        self.scipy_kwargs = scipy_kwargs
+        self.height = height
+
+    def transform(self, X, y=None, **fit_params):
+        peaks, properties = scipy.signal.find_peaks(X, height=self.height,
+                                                    **self.scipy_kwargs)
+        # TODO sort peaks by their height.
+        return peaks
+
+    def fit(self, X, y=None, **fit_params):
+        return self
 
 
 if __name__ == "__main__":
