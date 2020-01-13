6bea02e3adf1a97d0871749335979166782061b7
Raquel <raquel2terrero@gmail.com>
2020-01-13 21:28:56 +0100

    add TimeSeriesClassifier classes

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from statistics import mode
+import abc
 
 import numpy as np
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin
 
 from dataset import HARDataset
 
@@ -18,15 +19,50 @@ def classify_time_series(ts, clf, window_length, metadata=None):
     :type metadata: list or None.
     """
     serie = []
-    for w in range(0, ts.shape[0], window_length):
-        crop = ts[w:(w + window_length)]
+    for i, w in enumerate(range(0, ts.shape[0], window_length)):
+        if (i + 1) * window_length > ts.shape[0]:
+            break
+        crop_ = ts[w:(w + window_length)]
+        crop = crop_.T.reshape((1, -1))
         if metadata is not None:
-            crop = np.concatenate([crop, metadata])
-        if crop.shape[0] == (window_length if metadata is None
-                             else window_length + len(metadata)):
-            serie.append(clf.predict(crop.reshape((1, -1))).item())
+            crop = np.hstack([crop, metadata])
+        serie.append(clf.predict(crop.reshape((1, -1))).item())
 
     return mode(serie)
+
+
+class TimeSeriesClassifier(metaclass=abc.ABCMeta):
+    def __init__(self, classifier, window_length):
+        super().__init__()
+        self.window_length = window_length
+        self.classifier = classifier
+
+    def predict(self, dataset):
+        y_pred = []
+        for x, _, meta in dataset:
+            x = np.hstack([x[:, :-6], np.linalg.norm(x[:, -6:-3], axis=1, keepdims=True),
+                           np.linalg.norm(x[:, -3:], axis=1, keepdims=True)])
+            y_pred.append(classify_time_series(x, self.classifier, 256, [meta]))
+        return y_pred
+
+    @abc.abstractmethod
+    def classify_time_series(self, ts, clf, window_length, metadata=None):
+        pass
+
+
+class ModeTimeSeriesClassifier(TimeSeriesClassifier):
+    def classify_time_series(self, ts, clf, window_length, metadata=None):
+        serie = []
+        for i, w in enumerate(range(0, ts.shape[0], window_length)):
+            if (i + 1) * window_length > ts.shape[0]:
+                break
+            crop_ = ts[w:(w + window_length)]
+            crop = crop_.T.reshape((1, -1))
+            if metadata is not None:
+                crop = np.hstack([crop, metadata])
+            serie.append(clf.predict(crop.reshape((1, -1))).item())
+
+        return mode(serie)
 
 
 def make_sklearn_dataset(dataset):
